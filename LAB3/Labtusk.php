b5c6b8f4cef2e301e31fa8272555f5651c2d36a7
Ayon ballav <115283734+AYONBALLAV@users.noreply.github.com>
2024-11-11 16:14:22 +0600

    Add files via upload

---
+++ LAB3/Labtusk.php
@@ -0,0 +1,8 @@
+<?php
+$length = 5;
+$width = 6;
+$area = $length * $width;
+$perimeter = 2*($length + $width);
+echo "The area:  " . $area ."<br>";
+echo "The perimeter:  " . $perimeter. "<br";
+?>
---
+++ LAB3/Labtusk2.php
@@ -0,0 +1,6 @@
+<?php
+ $vat =0.15;
+$income = 10000;
+$v = $vat * $income;
+echo "The VAT is: ". $v;
+?>
